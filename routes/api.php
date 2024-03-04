df12dddf92148246fc104b1cf186be53fadc0af8
Jasz-17 <jaszapata1983@gmail.com>
2024-03-04 09:41:32 +0100

    fix pagination route

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/destinations',[DestinationController::class,'index']);
+Route::get('/',[DestinationController::class,'index']);
