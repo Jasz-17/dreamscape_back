bdc3a0c9edd8ecfbe8c6b869335251c27ead39b8
Jasz-17 <jaszapata1983@gmail.com>
2024-03-07 16:33:27 +0100

    merge pagination branch

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::middleware(['cors'])->group(function () {
-Route::get('/',[DestinationController::class,'index']);
+Route::get('/',[DestinationController::class,'index']); 
 });
 
 Route::get('/destinations/{id}', [DestinationController::class, 'show']);
